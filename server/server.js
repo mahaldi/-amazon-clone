1495200e863deb20e62f26bdcd37dd7e77a9aa1e
mahaldiWS_ <aldi@umroh.com>
2020-05-21 16:39:01 +0700

    consume category route

--- server/server.js
+++ server/server.js
@@ -25,7 +25,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 //apis
 const productRoutes = require('./routes/product')
+const categoryRoutes = require('./routes/category')
+
 app.use('/api', productRoutes)
+app.use('/api', categoryRoutes)
 
 app.listen(3000, err => {
 	if (err)
