7392fffeca1b6ee2a21aaf128ab0a97c873d46f7
mahaldiWS_ <aldi@umroh.com>
2020-05-21 13:10:55 +0700

    consume mongoose

--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 const express = require('express') //https://idjs.github.io/belajar-nodejs/expressjs/index.html untuk bisa akses method http request
 const morgan = require('morgan') // untuk menampilkan request http pada terminal
 const bodyParser = require('body-parser') // agar data yang diberikan dari front end ke backend dalam format yang proper (translate data dari frontend ke backend)
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express()
+
+mongoose.connect(
+	process.env.DATABASE,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	err => {
+		if(err)
+			return console.log(err)
+		console.log('connected to database')
+	}
+)
 
 //middleware
 app.use(morgan('dev')) // menampilkan logs http request pada terminal
@@ -20,5 +34,5 @@ app.get('/', (req, res) => {
 app.listen(3000, err => {
 	if (err)
 		return console.log(err)
-	console.log('linstening on port 3000')
+	console.log('listening on port 3000')
 })
