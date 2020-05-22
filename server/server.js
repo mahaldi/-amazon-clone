322927c479632ad67ba67d242e83ce152459293b
mahaldiWS_ <aldi@umroh.com>
2020-05-22 11:19:59 +0700

    consume cors

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const morgan = require('morgan') // untuk menampilkan request http pada terminal
 const bodyParser = require('body-parser') // agar data yang diberikan dari front end ke backend dalam format yang proper (translate data dari frontend ke backend)
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const cors = require('cors') // jika tidak menggunakan ini maka ketika melakukan http request(ex:POST) di client akan muncul error cors
 
 dotenv.config()
 
@@ -22,6 +23,7 @@ mongoose.connect(
 app.use(morgan('dev')) // menampilkan logs http request pada terminal
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 
 //apis
 const productRoutes = require('./routes/product')
